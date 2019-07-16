0dcba0cda97573f759313f090a030a8ff9a5245f
Admin <Admin@DESKTOP-L24O6I8>
2019-07-16 10:31:00 -0600

    Cambios en ejecutar

--- src/main/java/vista/Ejecutar.java
+++ src/main/java/vista/Ejecutar.java
@@ -12,5 +12,6 @@ package vista;
 public class Ejecutar {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
+        System.out.println("Hola a todos");
     }
 }
